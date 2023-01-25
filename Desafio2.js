568fa42402f9c132e5850c1707a55d5f0c125002
TheCamioPedrow <102632074+TheCamioPedrow@users.noreply.github.com>
2023-01-24 22:54:46 -0300

    Update Desafio2.js

--- Desafio2.js
+++ Desafio2.js
@@ -1,82 +1,178 @@
-const fs = require('fs')
+const fs = require ("fs")
 
-const path = './Products.json'
+/* Importo la libreria de file system */
 
-class ProductManager{
-    escrituraDeData = async (data) =>{
-        try{
-            await fs.promises.writeFile (path, JSON.stringify(data, null, 2))
-        }catch{
-            console.log(error)
-        }
-    }
-    getProducts = async () => {
-        if (fs.existsSync(path)) {
-            const data = await fs.promises.readFile(path, 'utf-8');
-            const products = JSON.parse(data);
-            return products;
-        }
-        else{
-            console.log([])
-            return []
-        }
-    }
-    addProducts = async(obj) =>{
-        const product = await this.getProducts()
-        let id;
-        if (product.length === 0) {
-            id = 1
-            let newObj = {...obj, id: id}
-            let codes = product.map(productos => productos.code)
-            if (codes.includes(obj.code)) {
-                console.log('El código ya ha sido utilizado')
-            }else{ 
-                product.push(newObj)
-            } 
-        }else{
-            id = product[product.length-1].id+1;
-            let newObj = {...obj, id: id}
-            let codes = product.map(productos => productos.code)
-            if (codes.includes(obj.code)) {
-                console.log('El código ya ha sido utilizado')
-            }else{ 
-                product.push(newObj)
-            } 
-        }
-        await fs.promises.writeFile(path,JSON.stringify(product));
-        return obj
-    }
-    getProductsById = async (id) =>{
-        const productos = await this.getProducts()
-        if(productos[id-1]){
-        return console.log(productos[id-1])  
-        }else{
-            console.log('no encontrado')
-        }
-    }
-    deleteById = async (id) =>{
-        let productos = await this.getProducts()
-        let ids = productos.map(prod => prod.id) 
-        if (ids.includes(id)) {
-            try{
-                productos = productos.filter (producto => producto.id != id)
-                await fs.promises.writeFile(path,JSON.stringify(productos));
-            }catch(error){
-                console.log(error)
-            }
-        }else{
-            console.log('no encontrado')
-        }
-    }
+const writeFile = (path, products) =>
+	fs.promises.writeFile(path, JSON.stringify({ products: products }))
 
-}
-let instancia = new ProductManager()
-   instancia.addProducts({
-    "title": "papas",
-    "description": "son papas y sirven para comer",
-    "price": "300",
-    "thumbnail": "thumbsnail 1",
-    "code": "codigo 1",
-    "stock": 123,
+/* creamos la funcion write file para no tener que repetir codigo mas adelante. Sirve para escribir archivo y lo pasa a json */
+
+const readFile = async (path) => {
+    const asyncGetProducts = await fs.promises.readFile(path);
+    const parseResult = JSON.parse(asyncGetProducts);
     
-}) 
+    return parseResult;
+}
+
+/* creamos esta funcion para no tener que repertir mas adelante. Sirve para pasar de string a .json  */
+
+class ProductManager {
+    constructor(path){
+        this.path = path
+        this.products = []
+    }
+
+    /* Path es la ruta para donde queremos que se cree, modifique, busque o elimine nuestro archivo */
+
+    initialize = async () => {
+		const existsFile = fs.existsSync(this.path);
+
+		if (existsFile) {
+			console.log("El archivo ya existía!");
+			const { products } = await readFile(this.path);
+			this.products = products;
+		} else {
+			await writeFile(this.path, this.products);
+			console.log("El archivo se creó exitosamente!");
+		}
+	};
+
+    /* este metodo sirve para que si el archivo existe me avise que esta y sino que me cree el archivo. */
+
+    getProducts= async() =>{
+        const fileData = await readFile(this.path);
+        return fileData
+    }
+
+    /* este metodo me trae todos los productos */
+
+    addProducts = async({title, desc, price, thumbnail, code, stock}) => {
+
+        const existingProduct = this.products.find((product) => product.code === code || product.title === title);
+        
+        if (existingProduct) {
+            console.log("Error, ya existe un producto con ese codigo o titulo");
+        }else {
+
+        this.products.push({
+            id: ++this.id,
+            title,
+            desc,
+            price,
+            thumbnail,
+            code,
+            stock
+            });
+        await writeFile(this.path, this.products)
+        console.log("Se ha creado un producto")
+        }
+    }
+
+    getProductsById(id){
+        const findedProduct = this.products.find((product)=> product.id === id)
+
+        if(findedProduct){
+            return findedProduct
+        }else{
+            console.log("No se encuentra un producto con ese id")
+        }
+    }
+}
+
+updateProduct = async (id, newProduct) => {
+    const findIndexProduct = this.products.findIndex(
+        (product) => product.id === id
+    )
+
+    if (findIndexProduct !== -1) {
+        const id = this.products[findIndexProduct].id;
+
+        this.products[findIndexProduct] = {
+            id,
+            ...newProduct,
+        }
+        await writeFile(this.path, this.products)
+        console.log("Actualizado correctamente")
+    } else {
+        console.log("No se encuentra un producto con ese id")
+    }
+}
+
+deleteProduct = async (id) => {
+    const findIndexProduct = this.products.findIndex(
+        (product) => product.id === id
+    )
+
+    if (findIndexProduct !== -1) {
+        const newProducts = this.products.filter(
+            (product) => product.id !== id
+        )
+        await writeFile(this.path, newProducts)
+        console.log("Eliminado correctamente")
+    } else {
+        console.log("No se encuentra un producto con ese id");
+    }
+}
+
+
+async function main(){
+    const productManager = new ProductManager("./data.json")
+    await productManager.initialize()
+
+    let products = await productManager.getProducts()
+
+    console.log(products)
+
+    const newProduct = {
+        title: "P1",
+        desc: "D1",
+        price: "P1",
+        thumbnail: "T1",
+        code:"C1",
+        stock: "S1"
+    }
+
+    await productManager.addProducts(newProduct)
+
+    products = await productManager.getProducts()
+
+    console.log(products)
+}
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+/* const ProductManager = new ProductManager()
+
+const newProduct1 = {
+    title: "P1",
+    desc: "D1",
+    price: "P1",
+    thumbnail: "T1",
+    code:"C1",
+    stock: "S1"
+}
+
+const newProduct2 = {
+	title: "P2",
+	desc: "D2",
+	price: "P2",
+	thumbnail: "T2",
+	code: "C2",
+	stock: "S2",
+};
+
+ProductManager.addProducts(newProduct1)
+ProductManager.addProducts(newProduct2)
+
+
+console.log(ProductManager.getProducts) */
